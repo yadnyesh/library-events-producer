c1954e838e3c7a9318a1e8d5703e02f29336a379
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-04-20 12:30:52 +0530

    Initial Project Setup

---
+++ src/main/java/io/yadnyesh/libraryeventsproducer/LibraryEventsProducerApplication.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.libraryeventsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsProducerApplication.class, args);
+	}
+}
